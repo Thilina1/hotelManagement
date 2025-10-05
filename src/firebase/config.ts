0c77e9498eae752f858461e63579ff1d5575f393
Thilina Weerasinghe <yelmhms01@gmail.com>
2025-10-05 06:02:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-317577973-f696c",
+  "appId": "1:909219664895:web:f4d8b035ead3ac1e21cdca",
+  "apiKey": "AIzaSyDOqaqhI9l7xYCtwFbeSAUofNDBkDz1kGo",
+  "authDomain": "studio-317577973-f696c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "909219664895"
+};
